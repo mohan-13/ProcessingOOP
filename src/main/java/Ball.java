d73cd65cb97f8a8ba01faf39d45d4b4f8470f4f0
Mohankumar <mohan.17cs@kct.ac.in>
2021-02-24 10:24:02 +0530

    Added Functionalities of Ball

---
+++ src/main/java/Ball.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Ball {
+    public static final int DIAMETER = 10;
+    private PApplet sketch;
+    private int ballPosition;
+    private int ballSpeed;
+    private static int counter;
+
+    public Ball(PApplet sketch, int ballPosition, int ballSpeed) {
+        this.sketch = sketch;
+        this.ballPosition = ballPosition;
+        this.ballSpeed = ballSpeed;
+    }
+
+    public static void step()
+    {
+        counter++;
+    }
+    public void showBall()
+    {
+        sketch.ellipse(ballSpeed*counter,ballPosition,DIAMETER, DIAMETER);
+    }
+}

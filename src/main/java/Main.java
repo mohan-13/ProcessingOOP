2db0cd6884a8e563effc8141f505325c5a547a46
Mohankumar <mohan.17cs@kct.ac.in>
2021-02-24 10:17:11 +0530

    Added Main Class with PApplet Settings

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 640;
+
+    public static void main(String[] args) {
+        PApplet.main("Main",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+}
